1ff99365b7092004b1fe620976d1b734b89ce8fc
thibault <thibault.barbie@free.fr>
2018-03-05 09:02:31 +0900

    Added the generation of the dataset

---
+++ src/main.rs
@@ -0,0 +1,109 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
+
+extern crate rrt;
+
+extern crate rand;
+use rand::distributions::{IndependentSample,Range};
+
+use std::fs::File;
+use std::io::prelude::*;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ProbSol {
+    x:  Vec<f64>,
+    xi: Vec<f64>,
+}
+
+
+fn is_colliding(x: &Vec<f64>, p: &Vec<f64>, n_dim: usize,
+                n_obs: usize, collision_limit: f64 ) -> bool {
+
+    let squared_collision_limit = collision_limit.powf(2.0);
+    
+    for i in 0..n_obs as usize{
+        let mut tmp=0.0;
+        for j in (2+i)*n_dim..(3+i)*n_dim as usize{
+            tmp+=(x[j]-p[j-(2+i)*n_dim as usize]).powf(2.0);
+        }
+        if tmp<squared_collision_limit{
+            return true
+        }
+    }
+    
+    false
+}
+
+
+fn dataset_generation(n_data: usize, n_obs: usize, collision_limit: f64){
+    let mut rng = rand::thread_rng();
+    let mut dataset : Vec<ProbSol> = Vec::new(); 
+
+    while dataset.len() < n_data {
+        // x
+        let x_range = Range::new(0.001, 0.999);
+        let mut feasible_problem = false;
+        let mut x: Vec<f64> = vec![0.0;2*(2+n_obs)];
+        while !feasible_problem {
+            for i in 0..(2*(2+n_obs) as usize) {
+                x[i]=x_range.ind_sample(&mut rng);
+            }
+
+            feasible_problem = !(is_colliding(&x,&vec![x[0],x[1]],2,n_obs,collision_limit)||
+                                 is_colliding(&x,&vec![x[2],x[3]],2,n_obs,collision_limit));
+        }
+
+        // xi
+        let result = rrt::dual_rrt_connect(&vec![x[0],x[1]],
+                                           &vec![x[2],x[3]],
+                                           |p: &[f64]| !((p[0]-0.5).abs() < 0.2 &&
+                                                         (p[1]-0.5).abs() < 0.2 ),
+                                           || {
+                                               let between = Range::new(0.0, 1.0);
+                                               let mut rng = rand::thread_rng();
+                                               vec![between.ind_sample(&mut rng),
+                                                    between.ind_sample(&mut rng)]
+                                           },
+                                           0.05,
+                                           1000);
+        match result {
+            Err(e) => println!("Error : {}",e),
+            Ok(traj) => { let n=traj.len() as u32;
+                          if n>7 {
+                              let xi=vec![traj[(n/6) as usize][0],traj[(n/6) as usize][1],
+                                          traj[(2*n/6)as usize][0],traj[(2*n/6)as usize][1],
+                                          traj[(3*n/6)as usize][0],traj[(3*n/6)as usize][1],
+                                          traj[(4*n/6)as usize][0],traj[(4*n/6)as usize][1],
+                                          traj[(5*n/6)as usize][0],traj[(5*n/6)as usize][1]];
+                              dataset.push(ProbSol{x: x,xi :xi});
+                          }
+                          else {println!("Trajectory found too short")};
+            },
+        }
+    }
+    let serialized = serde_json::to_string(&dataset).unwrap();
+    let mut file = File::create("dataset.dat").unwrap();
+    file.write_all(serialized.into_bytes().as_slice()).unwrap();
+}
+
+fn load_dataset() -> Vec<ProbSol> {
+    let mut file = File::open("dataset.dat").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let dataset: Vec<ProbSol> = serde_json::from_str(&contents).unwrap();
+    dataset
+    
+}
+
+fn main() {
+    let n_obs=10;
+    let collision_limit=0.05;
+    let n_data = 10000;
+    let generate = false;
+
+    if generate {dataset_generation(n_data,n_obs,collision_limit);}
+    let dataset = load_dataset();
+    println!("{}",dataset.len())
+}
