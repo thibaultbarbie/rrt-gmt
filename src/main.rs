6d884aee8e60306beb3db6197295e5653d260307
thibault <thibault.barbie@free.fr>
2018-03-06 13:58:25 +0900

    Changed the save_gmm function to save the optimized parameters

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
-
 extern crate rand;
 
 extern crate rusty_machine;
@@ -20,23 +19,31 @@ pub struct ProbSol {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Ellipsoid {
-    mean: Vec<f64>,
-    covariance: Vec<f64>,
-    weight: f64,
+struct SerEllipsoid {
+    mu : Vec<f64>,
+    phi : Vec<f64>,
+    beta : f64,
 }
+
 
 mod dataset; 
 
 fn save_gmm(gmm : &GaussianMixtureModel, n_gaussians: usize) {
     println!("Saving the Gaussian Mixture Model parameters");
+    
+    // useful for the determinant computation
+    let dim = gmm.means().unwrap().select_rows(&[0]).into_vec().len();
 
     let weights = gmm.mixture_weights().data().to_vec();
 
     for i in 0..n_gaussians {
-        let e = Ellipsoid{mean: gmm.means().unwrap().select_rows(&[i]).clone().into_vec(),
-                          covariance: gmm.covariances().unwrap().clone()[0].clone().into_vec(),
-                          weight: weights[i]};
+        let phi = gmm.covariances().unwrap()[i].inverse().unwrap().into_vec();
+        let beta = 2.0*weights[i].ln()-
+            (6.283185 as f64).powi(dim as i32)*gmm.covariances().unwrap()[i].det();
+        
+        let e = SerEllipsoid{mu: gmm.means().unwrap().select_rows(&[i]).clone().into_vec(),
+                          phi: phi,
+                          beta: beta};
         let serialized = serde_json::to_string(&e).unwrap();
         let filename = "approximations/ellipsoid_".to_string()+&i.to_string()+".dat"; 
         let mut file = File::create(filename).unwrap();
@@ -49,28 +56,29 @@ fn main() {
     let collision_limit=0.05;
     let n_data = 1_000;
     let generate = true;
+    let training_gmm = false;
     let n_gaussians =2;
     
     // Dataset generation
     if generate {dataset::dataset_generation(n_data,n_obs,collision_limit);}
 
     
-    // Dataset loading
-    let dataset = dataset::load_dataset();
-    println!("Dataset loaded");
-    
     // Training
+    if training_gmm {
+        let dataset = dataset::load_dataset();
+        println!("Dataset loaded");
+    
+        let dataset_as_vector : Vec<f64> = dataset.iter().
+            fold(Vec::new(),|mut v, d| {v.extend(&d.x);
+                                        v.extend(&d.xi);
+                                        v });    
 
-    let dataset_as_vector : Vec<f64> = dataset.iter().
-        fold(Vec::new(),|mut v, d| {v.extend(&d.x);
-                                    v.extend(&d.xi);
-                                    v });    
-
-    println!("Beginning of GMT training");
-    let mut gmm = GaussianMixtureModel::new(n_gaussians);
-    match gmm.train(&Matrix::new(dataset.len(), 2*(2+n_obs)+2*5, dataset_as_vector)) {
-        Err(e) => println!("{}",e),
-        Ok(_) => {save_gmm(&gmm,n_gaussians)}
+        println!("Beginning of GMT training");
+        let mut gmm = GaussianMixtureModel::new(n_gaussians);
+        match gmm.train(&Matrix::new(dataset.len(), 2*(2+n_obs)+2*5, dataset_as_vector)) {
+            Err(e) => println!("{}",e),
+            Ok(_) => {save_gmm(&gmm,n_gaussians)}
+        }
     }
 
 }
